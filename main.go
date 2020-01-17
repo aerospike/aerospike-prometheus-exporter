7e51021dd52ab4da1de9f6911f3248ff3facf1e5
Khosrow Afroozeh <parshua@gmail.com>
2020-01-17 11:12:52 +0100

    Simplify logging for now, and correct the program's official name

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"log"
 	"net/http"
@@ -14,7 +13,7 @@ import (
 )
 
 var (
-	bind        = flag.String("b", ":9145", "AeroProm bind address")
+	bind        = flag.String("b", ":9145", "Bind address to listen for Prometheus")
 	host        = flag.String("h", "127.0.0.1", "Aerospike server seed hostname or IP address")
 	port        = flag.Int("p", 3000, "Aerospike server seed hostname or IP address port number")
 	user        = flag.String("U", "", "User name")
@@ -29,7 +28,6 @@ var (
 	resolution  = flag.Int("r", 5, "Database info calls (seconds)")
 	tags        = flag.String("tags", "", "Tags to pass to prometheus in labels. Useful for querying for grafana or alerting")
 
-	logger   *log.Logger
 	fullHost string
 )
 
@@ -42,9 +40,7 @@ func main() {
 
 	fullHost = *host + ":" + strconv.Itoa(*port)
 
-	var buf bytes.Buffer
-	logger = log.New(&buf, "", log.LstdFlags|log.Lshortfile)
-	logger.SetOutput(os.Stdout)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	host := aero.NewHost(*host, *port)
 	host.TLSName = *nodeTLSName
@@ -58,7 +54,7 @@ func main() {
 
 	http.Handle("/metrics", promhttp.HandlerFor(promReg, promhttp.HandlerOpts{}))
 
-	log.Println("Welcome to AeroProm, Aerospike's Prometheus exporter!")
+	log.Println("Welcome Aerospike's Prometheus exporter!")
 	log.Printf("Listening for Prometheus on: %s\n", *bind)
 	log.Fatalln(http.ListenAndServe(*bind, nil))
 }
