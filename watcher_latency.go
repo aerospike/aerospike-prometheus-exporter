9680f60010ab8adbfb67342fd064c276eb4b2fd7
spkesan <kesan7ton@gmail.com>
2020-08-26 17:02:00 +0530

    (Minor) Fix comment

--- watcher_latency.go
+++ watcher_latency.go
@@ -35,11 +35,11 @@ func (lw *LatencyWatcher) refresh(infoKeys []string, rawMetrics map[string]strin
 	for namespaceName, nsLatencyStats := range latencyStats {
 		for operation, opLatencyStats := range nsLatencyStats {
 			for i, labelValue := range opLatencyStats.(StatsMap)["bucketLabels"].([]string) {
-				// aerospike_latencies_<operation>_bucket metric - Less than or equal to histogram buckets
+				// aerospike_latencies_<operation>_<timeunit>_bucket metric - Less than or equal to histogram buckets
 				pm := makeMetric("aerospike_latencies", operation+"_"+opLatencyStats.(StatsMap)["timeUnit"].(string)+"_bucket", mtGauge, config.AeroProm.MetricLabels, "cluster_name", "service", "ns", "le")
 				ch <- prometheus.MustNewConstMetric(pm.desc, pm.valueType, opLatencyStats.(StatsMap)["bucketValues"].([]float64)[i], rawMetrics["cluster-name"], rawMetrics["service"], namespaceName, labelValue)
 
-				// aerospike_latencies_<operation>_count metric
+				// aerospike_latencies_<operation>_<timeunit>_count metric
 				if i == 0 {
 					pm = makeMetric("aerospike_latencies", operation+"_"+opLatencyStats.(StatsMap)["timeUnit"].(string)+"_count", mtGauge, config.AeroProm.MetricLabels, "cluster_name", "service", "ns")
 					ch <- prometheus.MustNewConstMetric(pm.desc, pm.valueType, opLatencyStats.(StatsMap)["bucketValues"].([]float64)[i], rawMetrics["cluster-name"], rawMetrics["service"], namespaceName)
