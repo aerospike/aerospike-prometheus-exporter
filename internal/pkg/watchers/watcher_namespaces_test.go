9dbdb0409e754ec2ca9fede0c5670711728dbc26
mphanias <pmokrala@aerospike.com>
2023-11-13 19:48:06 +0530

    OM147

--- internal/pkg/watchers/watcher_namespaces_test.go
+++ internal/pkg/watchers/watcher_namespaces_test.go
@@ -81,103 +81,18 @@ func runTestcase(t *testing.T) {
 	config.InitGaugeStats(unittests.GetConfigfileLocation(unittests.DEFAULT_GAUGE_LIST_FILE))
 
 	// rawMetrics := getRawMetrics()
-	nsWatcher := new(NamespaceWatcher)
+	nsWatcher := &NamespaceWatcher{}
 
 	// simulate, as if we are sending requestInfo to AS and get the namespaces, these are coming from mock-data-generator
 	passOneKeys := nsWatcher.PassOneKeys()
 	passOneOutput, _ := data.GetProvider().RequestInfo(passOneKeys)
-	fmt.Println("TestPassTwoKeys: passOneOutput: ", passOneOutput)
 	passTwokeyOutputs := nsWatcher.PassTwoKeys(passOneOutput)
-	arrWatcherMetrics, err := data.GetProvider().RequestInfo(passTwokeyOutputs)
-	assert.Nil(t, err)
+	arrRawMetrics, err := data.GetProvider().RequestInfo(passTwokeyOutputs)
+	assert.Nil(t, err, "Error while NamespaceWatcher.PassTwokeys ")
+	assert.NotEmpty(t, arrRawMetrics, "Error while NamespaceWatcher.PassTwokeys, RawMetrics is EMPTY ")
 
-	fmt.Println(arrWatcherMetrics)
+	// check the output with NamespaceWatcher
+	nsWatcherMetrics, err := nsWatcher.Refresh(passTwokeyOutputs, arrRawMetrics)
+	assert.Nil(t, err, "Error while NamespaceWatcher.Refresh with passTwokeyOutputs ", passTwokeyOutputs)
+	assert.NotEmpty(t, nsWatcherMetrics, "Error while NamespaceWatcher.Refresh, WatcherMetrics is EMPTY ")
 }
-
-// 	err := nsWatcher.Refresh(passTwokeyOutputs, rawMetrics)
-
-// 	if err == nil {
-// 		// map of string ==> map["namespace/metric-name"]["<VALUE>"]
-// 		// map of string ==> map["namespace/metric-name"]["<Label>"]
-// 		//  both used to assert the return values from actual code against calculated values
-// 		lOutputValues := map[string]string{}
-// 		lOutputLabels := map[string]string{}
-
-// 		// reads data from the Prom channel and creates a map of strings so we can assert in the below loop
-// 		domore := 1
-
-// 		// mock_expected_filedump := []string{}
-
-// 		for domore == 1 {
-// 			select {
-
-// 			case nsMetric := <-ch:
-// 				description := nsMetric.Desc().String()
-// 				var protobuffer dto.Metric
-// 				err := nsMetric.Write(&protobuffer)
-// 				if err != nil {
-// 					fmt.Println(" unable to get metric ", description, " data into protobuf ", err)
-// 				}
-
-// 				metricValue := ""
-// 				metricLabel := fmt.Sprintf("%s", protobuffer.Label)
-// 				if protobuffer.Gauge != nil {
-// 					metricValue = fmt.Sprintf("%.0f", *protobuffer.Gauge.Value)
-// 				} else if protobuffer.Counter != nil {
-// 					metricValue = fmt.Sprintf("%.0f", *protobuffer.Counter.Value)
-// 				}
-
-// 				// Desc{fqName: "aerospike_namespac_memory_free_pct", help: "memory free pct", constLabels: {}, variableLabels: [cluster_name service ns]}
-// 				metricNameFromDesc := extractMetricNameFromDesc(description)
-// 				namespaceFromLabel := extractNamespaceFromLabel(metricLabel)
-// 				// namespaceFromLabel := extractLabelNameValueFromFullLabel(metricLabel)
-// 				labelString := stringifyLabel(metricLabel)
-
-// 				// key will be like namespace/<metric_name>, this we use this check during assertion
-// 				keyName := makeKeyname(namespaceFromLabel, labelString, true)
-// 				keyName = makeKeyname(keyName, metricNameFromDesc, true)
-
-// 				lOutputValues[keyName] = metricValue
-// 				lOutputLabels[keyName] = metricLabel
-
-// 				// to dump expected outputs to a file, so no-need-to regenerate every-time
-// 				// mock_expected_filedump = append(mock_expected_filedump, makeMetricReady2dump(namespaceFromLabel, metricNameFromDesc, description, metricValue))
-
-// 			case <-time.After(1 * time.Second):
-// 				domore = 0
-
-// 			} // end select
-// 		}
-
-// 		// loop each namespace and compare the label and value
-// 		arrNames := strings.Split(passOneKeyOutputs["namespaces"], ";")
-
-// 		for nsIndex := range arrNames {
-// 			tnsForNamespace := arrNames[nsIndex]
-// 			lExpectedMetricNamedValues, lExpectedMetricLabels := nsdg.createNamespaceWatcherExpectedOutputs(mas, tnsForNamespace, true)
-
-// 			for key := range lOutputValues {
-// 				expectedValues := lExpectedMetricNamedValues[key]
-// 				expectedLabels := lExpectedMetricLabels[key]
-// 				outputMetricValues := lOutputValues[key]
-// 				outpuMetricLabels := lOutputLabels[key]
-// 				// assert - only if the value belongs to the namespace we read expected values and processing
-// 				//  a "/" because namespace can be like test and test_on_shmem,
-// 				if strings.HasPrefix(key, tnsForNamespace+"/") {
-
-// 					assert.Contains(t, expectedValues, outputMetricValues)
-// 					assert.Contains(t, expectedLabels, outpuMetricLabels)
-// 				}
-// 			}
-
-// 		}
-// 	} else {
-// 		fmt.Println(" Failed Refreshing, error: ", err)
-// 	}
-
-// }
-
-// // to dump expected outputs to a file, so no-need-to regenerate every-time
-// // func makeMetricReady2dump(ns string, metric string, desc string, value string) string {
-// // 	return ns + "#" + metric + "#" + desc + "#" + value
-// // }
