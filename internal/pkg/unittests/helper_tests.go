5157efb45dc4a31cb014bc51104043fffd45ab93
mphanias <pmokrala@aerospike.com>
2023-11-13 19:05:05 +0530

    OM147

--- internal/pkg/unittests/helper_tests.go
+++ internal/pkg/unittests/helper_tests.go
@@ -18,7 +18,7 @@ func GetConfigfileLocation(filename string) string {
 	l_filename, _ := os.Getwd()
 
 	l_filename = l_filename + "/" + filename
-	fmt.Println("GetConfigfileLocation: ", l_filename)
+	fmt.Println("GetConfigfileLocation: ", l_filename, " \n\t os.Args[0]: ", os.Args[0])
 
 	return filename
 }
