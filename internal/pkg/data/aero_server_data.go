10a0680f010b0ca32b18144e213bc17d27aebfc1
mphanias <pmokrala@aerospike.com>
2023-11-14 13:14:42 +0530

    OM147

--- internal/pkg/data/aero_server_data.go
+++ internal/pkg/data/aero_server_data.go
@@ -21,6 +21,7 @@ type AerospikeServerProvider struct {
 }
 
 func (asm AerospikeServerProvider) RequestInfo(infoKeys []string) (map[string]string, error) {
+	fmt.Println("RequestInfo... ", infoKeys)
 	return fetchRequestInfoFromAerospike(infoKeys)
 }
 
