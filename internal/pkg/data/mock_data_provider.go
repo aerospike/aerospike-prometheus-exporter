b17030766ee7c15f3255a59b676dbee902691db1
mphanias <pmokrala@aerospike.com>
2023-11-21 11:10:22 +0530

    OM147

--- internal/pkg/data/mock_data_provider.go
+++ internal/pkg/data/mock_data_provider.go
@@ -95,7 +95,7 @@ func (md *MockAerospikeServer) Initialize() {
 	filePath := MOCK_DATA_FILE
 
 	if _, err := os.Stat(filePath); err != nil {
-		fmt.Println(filePath, " - File does not exist, may be running in Unit-test mode ")
+		fmt.Println(filePath, " - Running in Unit-test mode ")
 		filePath = "../../../" + filePath
 	}
 
