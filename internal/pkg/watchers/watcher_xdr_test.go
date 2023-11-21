92e43d77abe638fbf23a996776204fc76e176578
mphanias <pmokrala@aerospike.com>
2023-11-21 14:22:39 +0530

    OM147

--- internal/pkg/watchers/watcher_xdr_test.go
+++ internal/pkg/watchers/watcher_xdr_test.go
@@ -56,13 +56,13 @@ func Test_Xdr_PassTwoKeys(t *testing.T) {
 	assert.NotEmpty(t, passTwoOutputs)
 	assert.NotEmpty(t, expectedPassTwoOutputs)
 
-	fmt.Println("Test_Xdr_PassTwoKeys: passTwoOutputs: ", passTwoOutputs)
-	fmt.Println("Test_Xdr_PassTwoKeys: expectedPassTwoOutputs: ", expectedPassTwoOutputs)
+	// fmt.Println("Test_Xdr_PassTwoKeys: passTwoOutputs: ", passTwoOutputs)
+	// fmt.Println("Test_Xdr_PassTwoKeys: expectedPassTwoOutputs: ", expectedPassTwoOutputs)
 
-	// for idx := range expectedPassTwoOutputs {
-	// 	// assert each element returned by NamespaceWatcher exists in expected outputs
-	// 	assert.Contains(t, passTwoOutputs, expectedPassTwoOutputs[idx], " value exists!")
-	// }
+	for idx := range expectedPassTwoOutputs {
+		// assert each element returned by NamespaceWatcher exists in expected outputs
+		assert.Contains(t, passTwoOutputs, expectedPassTwoOutputs[idx], " value exists!")
+	}
 
 }
 
