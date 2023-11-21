93a9170e8d8700bc61db26cc10630f6c4a7da0ac
mphanias <pmokrala@aerospike.com>
2023-11-21 23:29:51 +0530

    OM147

--- internal/pkg/processors/asprom_impl_test.go
+++ internal/pkg/processors/asprom_impl_test.go
@@ -20,10 +20,10 @@ func Test_RefreshDefault(t *testing.T) {
 	config.InitConfig(tests_utils.GetConfigfileLocation(tests_utils.TESTS_DEFAULT_CONFIG_FILE))
 
 	initialize_prom()
-	// asMetrics := get_aerospike_stats()
+	asMetrics := get_aerospike_stats()
 
 	// generate and validate labels
-	// all_runTestcase(t, nil)
+	all_runTestcase(t, asMetrics)
 }
 
 /**
