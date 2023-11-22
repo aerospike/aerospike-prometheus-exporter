551b1d7ce8662eb3f09cdf12ebe5b4132cb48097
mphanias <pmokrala@aerospike.com>
2023-11-22 14:51:16 +0530

    OM147

--- internal/pkg/config/gauge_stats_test.go
+++ internal/pkg/config/gauge_stats_test.go
@@ -8,13 +8,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetGaugesNotEmpty(t *testing.T) {
-	fmt.Println("initializing GaugeMetrics ... TestGetGaugesNotEmpty")
-
+func initConfigs() {
 	// Initialize and validate Gauge config
 	l_cwd, _ := os.Getwd()
 	InitGaugeStats(l_cwd + "/../../../configs/gauge_stats_list.toml")
 
+}
+
+func TestGetGaugesNotEmpty(t *testing.T) {
+	fmt.Println("initializing GaugeMetrics ... TestGetGaugesNotEmpty")
+
+	// Initialize configs and gauges
+	initConfigs()
 	gaugeList := GaugeStatHandler
 
 	nslist := gaugeList.NamespaceStats
@@ -23,57 +28,52 @@ func TestGetGaugesNotEmpty(t *testing.T) {
 	assert.NotEmpty(t, nodelist)
 }
 
-// func TestGetGaugesCounts(t *testing.T) {
-// 	fmt.Println("initializing GaugeMetrics ... TestGetGaugesCounts")
+func TestGetGaugesCounts(t *testing.T) {
+	fmt.Println("initializing GaugeMetrics ... TestGetGaugesCounts")
 
-// 	// Initialize and validate Gauge config
-// 	gaugeList := new(GaugeStats)
+	// Initialize and validate Gauge config
+	initConfigs()
+	gaugeList := GaugeStatHandler
 
-// 	initGaugeStats(METRICS_CONFIG_FILE, gaugeList)
+	glist := gaugeList.NamespaceStats
+	assert.Equal(t, len(glist), 99)
 
-// 	glist := gaugeList.NamespaceStats
-// 	assert.Equal(t, len(glist), 99)
+	glist = gaugeList.NodeStats
+	assert.Equal(t, len(glist), 69)
 
-// 	glist = gaugeList.NodeStats
-// 	assert.Equal(t, len(glist), 69)
+	glist = gaugeList.SetsStats
+	assert.Equal(t, len(glist), 9)
 
-// 	glist = gaugeList.SetsStats
-// 	assert.Equal(t, len(glist), 9)
+	glist = gaugeList.SindexStats
+	assert.Equal(t, len(glist), 13)
 
-// 	glist = gaugeList.SindexStats
-// 	assert.Equal(t, len(glist), 13)
+	glist = gaugeList.XdrStats
+	assert.Equal(t, len(glist), 10)
 
-// 	glist = gaugeList.XdrStats
-// 	assert.Equal(t, len(glist), 10)
+}
 
-// }
+func TestIsAGaugeTrue(t *testing.T) {
+	fmt.Println("initializing GaugeMetrics ... TestIsAGaugeTrue")
 
-// func TestIsAGaugeTrue(t *testing.T) {
-// 	fmt.Println("initializing GaugeMetrics ... TestIsAGaugeTrue")
+	// Initialize and validate Gauge config
+	initConfigs()
+	gaugeList := GaugeStatHandler
 
-// 	// Initialize and validate Gauge config
-// 	gaugeList := new(GaugeStats)
+	assert.Equal(t, gaugeList.NamespaceStats["cache_read_pct"], true)
 
-// 	initGaugeStats(METRICS_CONFIG_FILE, gaugeList)
+	// exists = gaugeList.isGauge(CTX_NODE_STATS, "cluster_clock_skew_stop_writes_sec")
+	// assert.Equal(t, exists, true)
 
-// 	exists := false
+	// exists = gaugeList.isGauge(CTX_SINDEX, "entries_per_rec")
+	// assert.Equal(t, exists, true)
 
-// 	exists = gaugeList.isGauge(CTX_NAMESPACE, "cache_read_pct")
-// 	assert.Equal(t, exists, true)
+	// exists = gaugeList.isGauge(CTX_XDR, "recoveries_pending")
+	// assert.Equal(t, exists, true)
 
-// 	exists = gaugeList.isGauge(CTX_NODE_STATS, "cluster_clock_skew_stop_writes_sec")
-// 	assert.Equal(t, exists, true)
+	// exists = gaugeList.isGauge(CTX_SETS, "truncate_lut")
+	// assert.Equal(t, exists, true)
 
-// 	exists = gaugeList.isGauge(CTX_SINDEX, "entries_per_rec")
-// 	assert.Equal(t, exists, true)
-
-// 	exists = gaugeList.isGauge(CTX_XDR, "recoveries_pending")
-// 	assert.Equal(t, exists, true)
-
-// 	exists = gaugeList.isGauge(CTX_SETS, "truncate_lut")
-// 	assert.Equal(t, exists, true)
-
-// }
+}
 
 // func TestNoGaugeExists(t *testing.T) {
 
