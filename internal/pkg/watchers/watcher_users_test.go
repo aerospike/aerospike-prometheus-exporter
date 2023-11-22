e61c9a8a106d609af7d70504a277e96a1a04f99d
mphanias <pmokrala@aerospike.com>
2023-11-22 14:29:50 +0530

    OM147

--- internal/pkg/watchers/watcher_users_test.go
+++ internal/pkg/watchers/watcher_users_test.go
@@ -30,7 +30,7 @@ func Test_Users_PassTwoKeys(t *testing.T) {
 	fmt.Println("initializing config ... Test_Users_PassTwoKeys")
 
 	// initialize config and gauge-lists
-	tests_utils.InitConfigurations(tests_utils.TESTS_DEFAULT_CONFIG_FILE)
+	tests_utils.InitConfigurations(tests_utils.TESTS_USERS_CONFIG_FILE)
 
 	// Check passoneKeys
 	usersWatcher := &UserWatcher{}
@@ -53,7 +53,7 @@ func Test_Users_RefreshDefault(t *testing.T) {
 	fmt.Println("initializing config ... Test_Users_RefreshDefault")
 
 	// initialize config and gauge-lists
-	tests_utils.InitConfigurations(tests_utils.TESTS_DEFAULT_CONFIG_FILE)
+	tests_utils.InitConfigurations(tests_utils.TESTS_USERS_CONFIG_FILE)
 
 	users_runTestcase(t)
 }
