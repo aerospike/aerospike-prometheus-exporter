f70c5d96331b43ea02dd051b5483e3882845ea3e
mphanias <pmokrala@aerospike.com>
2024-01-25 21:57:52 +0530

    OM147 - mem info using procfs

--- internal/pkg/systeminfo/mem_info.go
+++ internal/pkg/systeminfo/mem_info.go
@@ -17,6 +17,72 @@ var (
 )
 
 func GetMemInfo() (map[string]float64, error) {
+	stats := GetMemInfoUsingProcFS()
+
+	return stats, nil
+}
+
+func GetMemInfoUsingProcFS() map[string]float64 {
+	fs, err := procfs.NewFS("/proc")
+	handleError(err)
+
+	meminfo, err := fs.Meminfo()
+	handleError(err)
+
+	memStats := make(map[string]float64)
+
+	memStats["Active"] = float64(*meminfo.Active)
+	memStats["ActiveAnon"] = float64(*meminfo.ActiveAnon)
+	memStats["ActiveFile"] = float64(*meminfo.ActiveFile)
+	memStats["AnonHugePages"] = float64(*meminfo.AnonHugePages)
+	memStats["Bounce"] = float64(*meminfo.Bounce)
+	memStats["Buffers"] = float64(*meminfo.Buffers)
+	memStats["Cached"] = float64(*meminfo.Cached)
+	memStats["CmaFree"] = float64(*meminfo.CmaFree)
+	memStats["CmaTotal"] = float64(*meminfo.CmaTotal)
+	memStats["CommitLimit"] = float64(*meminfo.CommitLimit)
+	memStats["CommittedAS"] = float64(*meminfo.CommittedAS)
+	memStats["DirectMap1G"] = float64(*meminfo.DirectMap1G)
+	memStats["DirectMap1G"] = float64(*meminfo.DirectMap1G)
+	memStats["DirectMap4k"] = float64(*meminfo.DirectMap4k)
+	memStats["Dirty"] = float64(*meminfo.Dirty)
+	memStats["HardwareCorrupted"] = float64(*meminfo.HardwareCorrupted)
+	memStats["HugePagesFree"] = float64(*meminfo.HugePagesFree)
+	memStats["HugePagesRsvd"] = float64(*meminfo.HugePagesRsvd)
+	memStats["HugePagesSurp"] = float64(*meminfo.HugePagesSurp)
+	memStats["HugePagesTotal"] = float64(*meminfo.HugePagesTotal)
+	memStats["Hugepagesize"] = float64(*meminfo.Hugepagesize)
+	memStats["Inactive"] = float64(*meminfo.Inactive)
+	memStats["Inactive"] = float64(*meminfo.Inactive)
+	memStats["InactiveFile"] = float64(*meminfo.InactiveFile)
+	memStats["KernelStack"] = float64(*meminfo.KernelStack)
+	memStats["Mapped"] = float64(*meminfo.Mapped)
+	memStats["MemAvailable"] = float64(*meminfo.MemAvailable)
+	memStats["MemFree"] = float64(*meminfo.MemFree)
+	memStats["MemTotal"] = float64(*meminfo.MemTotal)
+	memStats["Mlocked"] = float64(*meminfo.Mlocked)
+	memStats["NFSUnstable"] = float64(*meminfo.NFSUnstable)
+	memStats["PageTables"] = float64(*meminfo.PageTables)
+	memStats["SReclaimable"] = float64(*meminfo.SReclaimable)
+	memStats["SUnreclaim"] = float64(*meminfo.SUnreclaim)
+	memStats["Shmem"] = float64(*meminfo.Shmem)
+	memStats["ShmemHugePages"] = float64(*meminfo.ShmemHugePages)
+	memStats["ShmemPmdMapped"] = float64(*meminfo.ShmemPmdMapped)
+	memStats["Slab"] = float64(*meminfo.Slab)
+	memStats["SwapCached"] = float64(*meminfo.SwapCached)
+	memStats["SwapFree"] = float64(*meminfo.SwapFree)
+	memStats["SwapTotal"] = float64(*meminfo.SwapTotal)
+	memStats["Unevictable"] = float64(*meminfo.Unevictable)
+	memStats["VmallocChunk"] = float64(*meminfo.VmallocChunk)
+	memStats["VmallocTotal"] = float64(*meminfo.VmallocTotal)
+	memStats["VmallocUsed"] = float64(*meminfo.VmallocUsed)
+	memStats["Writeback"] = float64(*meminfo.Writeback)
+	memStats["WritebackTmp"] = float64(*meminfo.WritebackTmp)
+
+	return memStats
+}
+
+func GetMemInfoParsingFile() (map[string]float64, error) {
 	pathMeminfo := GetProcFilePath("meminfo")
 	fmt.Println("\t\t *** pathMeminfo : ", pathMeminfo)
 	file, err := os.Open(pathMeminfo)
@@ -26,24 +92,8 @@ func GetMemInfo() (map[string]float64, error) {
 	defer file.Close()
 
 	// testing
-	testingProcsFS()
 
 	return parseMemInfo(file)
-}
-
-func testingProcsFS() {
-	fs, err := procfs.NewFS("/proc")
-	handleError(err)
-	stats, err := fs.Stat()
-	handleError(err)
-	fmt.Println("\t\t ====> stats : ", stats.CPU)
-
-	// mem, err := procfs.NewFS(GetProcFilePath("meminfo"))
-	meminfo, err := fs.Meminfo()
-	handleError(err)
-	fmt.Println("\t\t Memory free: unit64 ", meminfo.MemFree)
-	fmt.Println("\t\t Memory free: float64 with pointer ", float64(*meminfo.MemFree))
-
 }
 
 func parseMemInfo(r io.Reader) (map[string]float64, error) {
