ee88b72551d3810203327411789f62d45480686b
mphanias <pmokrala@aerospike.com>
2024-01-30 16:38:09 +0530

    OM147 - system info

--- internal/pkg/dataprovider/system_provider.go
+++ internal/pkg/dataprovider/system_provider.go
@@ -5,35 +5,58 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func (sip SystemInfoProvider) GetCPUDetails() ([]CPUStatDetails, []CPUGuestStatDetails) {
+func (sip SystemInfoProvider) GetCPUDetails() ([]map[string]float64, []map[string]float64) {
 
-	arrCpuStatDetails := []CPUStatDetails{}
-	arrGuestCpuStatDetails := []CPUGuestStatDetails{}
+	arrGuestCpuStats := []map[string]float64{}
+	arrCpuStats := []map[string]float64{}
 
 	fs, err := procfs.NewFS(PROC_PATH)
 	if err != nil {
 		log.Debug("parseCpuStats Error while reading CPU Stats from ", PROC_PATH, " Error ", err)
+		return arrGuestCpuStats, arrCpuStats
 	}
 
 	stats, err := fs.Stat()
 	if err != nil {
 		log.Debug("Eror while reading procfs.NewFS system, error: ", err)
+		return arrGuestCpuStats, arrCpuStats
 	}
+	// guest_seconds_total:user:100;nice:10
+	// seconds_total:
 
 	for index, cpu := range stats.CPU {
 		// fmt.Println("parsing CPU stats ", index)
-		arrGuestCpuStatDetails = append(arrGuestCpuStatDetails, CPUGuestStatDetails{index, "guest_seconds_total", "user", cpu.Guest})
-		arrGuestCpuStatDetails = append(arrGuestCpuStatDetails, CPUGuestStatDetails{index, "guest_seconds_total", "nice", cpu.GuestNice})
+		guestCpuValues := make(map[string]float64)
+		guestCpuValues["index"] = float64(index)
+		guestCpuValues["user"] = cpu.Guest
+		guestCpuValues["nice"] = cpu.GuestNice
 
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "idle", cpu.Idle})
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "irq", cpu.IRQ})
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "iowait", cpu.Iowait})
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "nice", cpu.Nice})
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "soft_irq", cpu.SoftIRQ})
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "steal", cpu.Steal})
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "system", cpu.System})
-		arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "user", cpu.User})
+		cpuValues := make(map[string]float64)
+		cpuValues["index"] = float64(index)
+		cpuValues["user"] = cpu.Guest
+		cpuValues["idle"] = cpu.Idle
+		cpuValues["irq"] = cpu.IRQ
+		cpuValues["iowait"] = cpu.Iowait
+		cpuValues["nice"] = cpu.Nice
+		cpuValues["soft_irq"] = cpu.SoftIRQ
+		cpuValues["steal"] = cpu.Steal
+		cpuValues["system"] = cpu.System
+		cpuValues["user"] = cpu.User
+
+		arrGuestCpuStats = append(arrGuestCpuStats, guestCpuValues)
+		arrCpuStats = append(arrCpuStats, cpuValues)
+
+		// arrGuestCpuStatDetails = append(arrGuestCpuStatDetails, CPUGuestStatDetails{index, "guest_seconds_total", "user", cpu.Guest})
+		// arrGuestCpuStatDetails = append(arrGuestCpuStatDetails, CPUGuestStatDetails{index, "guest_seconds_total", "nice", cpu.GuestNice})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "idle", cpu.Idle})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "irq", cpu.IRQ})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "iowait", cpu.Iowait})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "nice", cpu.Nice})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "soft_irq", cpu.SoftIRQ})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "steal", cpu.Steal})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "system", cpu.System})
+		// arrCpuStatDetails = append(arrCpuStatDetails, CPUStatDetails{index, "seconds_total", "user", cpu.User})
 	}
 
-	return arrCpuStatDetails, arrGuestCpuStatDetails
+	return arrGuestCpuStats, arrCpuStats
 }
--- internal/pkg/systeminfo/si_cpu.go
+++ internal/pkg/systeminfo/si_cpu.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 
 	"github.com/aerospike/aerospike-prometheus-exporter/internal/pkg/commons"
+	"github.com/aerospike/aerospike-prometheus-exporter/internal/pkg/dataprovider"
 	"github.com/aerospike/aerospike-prometheus-exporter/internal/pkg/statprocessors"
-	"github.com/prometheus/procfs"
-	log "github.com/sirupsen/logrus"
 )
 
 func GetCpuInfo() []SystemInfoStat {
@@ -17,36 +16,29 @@ func GetCpuInfo() []SystemInfoStat {
 func parseCpuStats() []SystemInfoStat {
 	arrSysInfoStats := []SystemInfoStat{}
 
-	fs, err := procfs.NewFS(PROC_PATH)
-	if err != nil {
-		log.Debug("parseCpuStats Error while reading CPU Stats from ", PROC_PATH, " Error ", err)
-		return arrSysInfoStats
+	guestCpuDetails, cpuDetails := dataprovider.GetSystemProvider().GetCPUDetails()
+
+	for _, stat := range guestCpuDetails {
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("guest_seconds_total", fmt.Sprint(stat["index"]), "user", stat["user"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("guest_seconds_total", fmt.Sprint(stat["index"]), "nice", stat["nice"]))
 	}
 
-	stats, err := fs.Stat()
-	if err != nil {
-		log.Debug("Eror while reading procfs.NewFS system, error: ", err)
-		return arrSysInfoStats
-	}
-
-	for index, cpu := range stats.CPU {
+	for _, stat := range cpuDetails {
 		// fmt.Println("parsing CPU stats ", index)
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("guest_seconds_total", index, "user", cpu.Guest))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("guest_seconds_total", index, "nice", cpu.GuestNice))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "idle", cpu.Idle))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "irq", cpu.IRQ))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "iowait", cpu.Iowait))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "nice", cpu.Nice))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "soft_irq", cpu.SoftIRQ))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "steal", cpu.Steal))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "system", cpu.System))
-		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "user", cpu.User))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "idle", stat["idle"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "irq", stat["irq"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "iowait", stat["iowait"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "nice", stat["nice"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "soft_irq", stat["soft_irq"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "steal", stat["steal"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "system", stat["system"]))
+		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", fmt.Sprint(stat["index"]), "user", stat["user"]))
 	}
 
 	return arrSysInfoStats
 }
 
-func constructCpuStats(cpuStatName string, cpuNo int64, cpuMode string, value float64) SystemInfoStat {
+func constructCpuStats(cpuStatName string, cpuNo string, cpuMode string, value float64) SystemInfoStat {
 	clusterName := statprocessors.ClusterName
 	service := statprocessors.Service
 
@@ -63,3 +55,35 @@ func constructCpuStats(cpuStatName string, cpuNo int64, cpuMode string, value fl
 
 	return sysMetric
 }
+
+// func parseCpuStats() []SystemInfoStat {
+// 	arrSysInfoStats := []SystemInfoStat{}
+
+// 	fs, err := procfs.NewFS(PROC_PATH)
+// 	if err != nil {
+// 		log.Debug("parseCpuStats Error while reading CPU Stats from ", PROC_PATH, " Error ", err)
+// 		return arrSysInfoStats
+// 	}
+
+// 	stats, err := fs.Stat()
+// 	if err != nil {
+// 		log.Debug("Eror while reading procfs.NewFS system, error: ", err)
+// 		return arrSysInfoStats
+// 	}
+
+// 	for index, cpu := range stats.CPU {
+// 		// fmt.Println("parsing CPU stats ", index)
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("guest_seconds_total", index, "user", cpu.Guest))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("guest_seconds_total", index, "nice", cpu.GuestNice))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "idle", cpu.Idle))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "irq", cpu.IRQ))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "iowait", cpu.Iowait))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "nice", cpu.Nice))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "soft_irq", cpu.SoftIRQ))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "steal", cpu.Steal))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "system", cpu.System))
+// 		arrSysInfoStats = append(arrSysInfoStats, constructCpuStats("seconds_total", index, "user", cpu.User))
+// 	}
+
+// 	return arrSysInfoStats
+// }
