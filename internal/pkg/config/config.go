6c86b0a19bad073ce30060c1ea8ecd3b00a34425
mphanias <pmokrala@aerospike.com>
2024-02-02 14:54:48 +0530

    OM147 - configs

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -42,12 +42,7 @@ type Config struct {
 	} `toml:"Agent.Prom"`
 
 	AgentOtel struct {
-		OtelServiceName             string            `toml:"service_name"`
-		OtelEndpoint                string            `toml:"endpoint"`
-		OtelTlsEnabled              bool              `toml:"tls_enabled"`
-		OtelHeaders                 map[string]string `toml:"headers"`
-		OtelPushInterval            uint8             `toml:"push_interval"`
-		OtelServerStatFetchInterval uint8             `toml:"server_stat_fetch_interval"`
+		OtelServiceName string `toml:"service_name"`
 	} `toml:"Agent.Otel"`
 
 	Aerospike struct {
