17164989ad3aa3e6be0e54f42fa5e1e2aa9633be
mphanias <pmokrala@aerospike.com>
2024-08-15 23:44:03 +0530

    fixed code to handle metric-type of sysinfo

--- internal/pkg/statprocessors/utils.go
+++ internal/pkg/statprocessors/utils.go
@@ -143,6 +143,12 @@ func isGauge(pContextType commons.ContextType, pStat string) bool {
 		return config.GaugeStatHandler.XdrStats[pStat]
 	}
 
+	// any sysinfo_ check if it exists in gauge_stats_list.toml
+	if strings.Contains(strings.ToLower(string(pContextType)), "sysinfo_") {
+		fmt.Println("\t given stat ", pStat, " is a sysinfo... checking against gauge list. ", config.GaugeStatHandler.SysInfoStats[pStat])
+		return config.GaugeStatHandler.SysInfoStats[pStat]
+	}
+
 	return false
 }
 
