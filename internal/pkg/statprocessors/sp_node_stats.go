ab549d7ee2c2458c546c290074af488e2c52a12c
Phaniram Mokrala <121247041+mphanias@users.noreply.github.com>
2025-03-27 00:08:32 +0530

    OM-222 - Send pseudo metrics to represent log debug/detail levels (#138)

--- internal/pkg/statprocessors/sp_node_stats.go
+++ internal/pkg/statprocessors/sp_node_stats.go
@@ -1,6 +1,7 @@
 package statprocessors
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/aerospike/aerospike-prometheus-exporter/internal/pkg/commons"
@@ -11,23 +12,50 @@ import (
 const (
 	KEY_SERVICE_CONFIG     = "get-config:context=service"
 	KEY_SERVICE_STATISTICS = "statistics"
+	KEY_SERVICE_LOGS       = "logs"
 )
 
 type NodeStatsProcessor struct {
-	nodeMetrics map[string]AerospikeStat
+	nodeMetrics  map[string]AerospikeStat
+	logSinkCount int
 }
 
 func (sw *NodeStatsProcessor) PassOneKeys() []string {
-	log.Tracef("node-passonekeys:nil")
+	log.Tracef("node-passonekeys:logs")
 
-	return nil
+	return []string{KEY_SERVICE_LOGS}
 }
 
 func (sw *NodeStatsProcessor) PassTwoKeys(rawMetrics map[string]string) []string {
 	// we need to fetch both configs and stat
-	log.Tracef("node-passtwokeys:%s", []string{KEY_SERVICE_CONFIG, KEY_SERVICE_STATISTICS})
 
-	return []string{KEY_SERVICE_CONFIG, KEY_SERVICE_STATISTICS}
+	// if Logs are configure/present, send individual sink log commands
+	sinkCmds := sw.parseLogSinkDetails(rawMetrics)
+
+	passTwoKeys := []string{KEY_SERVICE_CONFIG, KEY_SERVICE_STATISTICS}
+	passTwoKeys = append(passTwoKeys, sinkCmds...)
+
+	log.Tracef("node-passtwokeys:%s", passTwoKeys)
+
+	return passTwoKeys
+}
+
+func (sw *NodeStatsProcessor) parseLogSinkDetails(rawMetrics map[string]string) []string {
+	logSinkCmds := []string{}
+
+	logSinks := strings.Split(rawMetrics[KEY_SERVICE_LOGS], ";")
+
+	// reset the logSinkCount to 0 always, if server restarts by changing debug level, no need to fetch
+	sw.logSinkCount = 0
+
+	// 0:stderr;1:/var/log/aerospike/aerospike.log
+	for _, logSink := range logSinks {
+		logSinkId := strings.Split(logSink, ":")
+		logSinkCmds = append(logSinkCmds, "log/"+logSinkId[0])
+		sw.logSinkCount++
+	}
+
+	return logSinkCmds
 }
 
 // All (allowed/blocked) node stats. Based on the config.Aerospike.NodeMetricsAllowlist, config.Aerospike.NodeMetricsBlocklist.
@@ -39,23 +67,24 @@ func (sw *NodeStatsProcessor) Refresh(infoKeys []string, rawMetrics map[string]s
 		sw.nodeMetrics = make(map[string]AerospikeStat)
 	}
 
-	nodeConfigs := rawMetrics[KEY_SERVICE_CONFIG]
-	nodeStats := rawMetrics[KEY_SERVICE_STATISTICS]
-	log.Tracef("node-configs:%s", nodeConfigs)
-	log.Tracef("node-stats:%s", nodeStats)
+	log.Tracef("node-configs:%s", rawMetrics[KEY_SERVICE_CONFIG])
+	log.Tracef("node-stats:%s", rawMetrics[KEY_SERVICE_STATISTICS])
 
 	// we are sending configs and stats in same refresh call, as both are being sent to prom, instead of doing prom-push in 2 functions
 	// handle configs
 	var allMetricsToSend = []AerospikeStat{}
 
-	lCfgMetricsToSend := sw.handleRefresh(nodeConfigs)
+	lCfgMetricsToSend := sw.handleRefresh(rawMetrics[KEY_SERVICE_CONFIG])
 
 	// handle stats
-	lStatMetricsToSend := sw.handleRefresh(nodeStats)
+	lStatMetricsToSend := sw.handleRefresh(rawMetrics[KEY_SERVICE_STATISTICS])
 
 	// merge both array into single
 	allMetricsToSend = append(allMetricsToSend, lCfgMetricsToSend...)
 	allMetricsToSend = append(allMetricsToSend, lStatMetricsToSend...)
+
+	// parse logs Sink
+	allMetricsToSend = append(allMetricsToSend, sw.handleLogSinkStats(rawMetrics)...)
 
 	return allMetricsToSend, nil
 }
@@ -109,4 +138,49 @@ func (sw *NodeStatsProcessor) handleRefresh(nodeRawMetrics string) []AerospikeSt
 	}
 
 	return refreshMetricsToSend
+}
+
+func (sw *NodeStatsProcessor) handleLogSinkStats(rawMetrics map[string]string) []AerospikeStat {
+
+	var refreshMetricsToSend = []AerospikeStat{}
+
+	debugValue := 0.0
+	detailValue := 0.0
+
+	// log-sink-ids will be from 0..(n-1)
+	for idx := 0; idx < sw.logSinkCount; idx++ {
+		logSinkKey := "log/" + strconv.Itoa(idx)
+		value := rawMetrics[logSinkKey]
+
+		if strings.Contains(value, ":DEBUG") {
+			debugValue = 1.0
+		}
+
+		if strings.Contains(value, ":DETAIL") {
+			detailValue = 1.0
+		}
+	}
+
+	refreshMetricsToSend = append(refreshMetricsToSend, sw.createLogSinkMetric("pseudo_log_debug", debugValue))
+	refreshMetricsToSend = append(refreshMetricsToSend, sw.createLogSinkMetric("pseudo_log_detail", detailValue))
+
+	return refreshMetricsToSend
+}
+
+func (sw *NodeStatsProcessor) createLogSinkMetric(statName string, statValue float64) AerospikeStat {
+	asMetric, exists := sw.nodeMetrics[statName]
+
+	if !exists {
+		allowed := isMetricAllowed(commons.CTX_NODE_STATS, statName)
+		asMetric = NewAerospikeStat(commons.CTX_NODE_STATS, statName, allowed)
+		sw.nodeMetrics[statName] = asMetric
+	}
+
+	labels := []string{commons.METRIC_LABEL_CLUSTER_NAME, commons.METRIC_LABEL_SERVICE}
+	labelValues := []string{ClusterName, Service}
+
+	asMetric.updateValues(statValue, labels, labelValues)
+
+	return asMetric
+
 }
