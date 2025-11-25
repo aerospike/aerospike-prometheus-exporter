40a8a3d7bd2ed5e897a2111b821a14d873e89f47
mphani <phaniram@gmail.com>
2025-11-25 11:23:38 +0530

    OM-265
    added constants
    added metric construction logic

--- internal/pkg/commons/constants.go
+++ internal/pkg/commons/constants.go
@@ -3,27 +3,32 @@ package commons
 import "github.com/prometheus/procfs"
 
 const (
-	CTX_USERS      ContextType = "users"
-	CTX_NAMESPACE  ContextType = "namespace"
-	CTX_NODE_STATS ContextType = "node_stats"
-	CTX_SETS       ContextType = "sets"
-	CTX_SINDEX     ContextType = "sindex"
-	CTX_XDR        ContextType = "xdr"
-	CTX_LATENCIES  ContextType = "latencies"
+	CTX_USERS       ContextType = "users"
+	CTX_NAMESPACE   ContextType = "namespace"
+	CTX_NODE_STATS  ContextType = "node_stats"
+	CTX_SETS        ContextType = "sets"
+	CTX_SINDEX      ContextType = "sindex"
+	CTX_XDR         ContextType = "xdr"
+	CTX_USER_AGENTS ContextType = "user_agents"
+	CTX_LATENCIES   ContextType = "latencies"
 )
 
 // below constant represent the labels we send along with metrics to Prometheus or something
 const (
-	METRIC_LABEL_CLUSTER_NAME   = "cluster_name"
-	METRIC_LABEL_SERVICE        = "service"
-	METRIC_LABEL_NS             = "ns"
-	METRIC_LABEL_SET            = "set"
-	METRIC_LABEL_LE             = "le"
-	METRIC_LABEL_DC_NAME        = "dc"
-	METRIC_LABEL_INDEX          = "index"
-	METRIC_LABEL_SINDEX         = "sindex"
-	METRIC_LABEL_STORAGE_ENGINE = "storage_engine"
-	METRIC_LABEL_USER           = "user"
+	METRIC_LABEL_CLUSTER_NAME              = "cluster_name"
+	METRIC_LABEL_SERVICE                   = "service"
+	METRIC_LABEL_NS                        = "ns"
+	METRIC_LABEL_SET                       = "set"
+	METRIC_LABEL_LE                        = "le"
+	METRIC_LABEL_DC_NAME                   = "dc"
+	METRIC_LABEL_INDEX                     = "index"
+	METRIC_LABEL_SINDEX                    = "sindex"
+	METRIC_LABEL_STORAGE_ENGINE            = "storage_engine"
+	METRIC_LABEL_USER                      = "user"
+	METRIC_LABEL_UA_VERSION                = "version"
+	METRIC_LABEL_UA_CLIENT_LIBRARY_VERSION = "client_library_version"
+	METRIC_LABEL_UA_APP_ID                 = "app_id"
+	METRIC_LABEL_EXPORTER_VERSION          = "exporter_version"
 )
 
 // constants used to identify type of metrics
--- internal/pkg/statprocessors/sp_user_agents.go
+++ internal/pkg/statprocessors/sp_user_agents.go
@@ -1,8 +1,12 @@
 package statprocessors
 
 import (
+	"encoding/base64"
 	"fmt"
+	"strings"
 
+	"github.com/aerospike/aerospike-prometheus-exporter/internal/pkg/commons"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -11,33 +15,114 @@ const (
 )
 
 type UserAgentsStatsProcessor struct {
-	xdrMetrics map[string]AerospikeStat
+	userAgentsMetrics map[string]AerospikeStat
 }
 
-func (xw *UserAgentsStatsProcessor) PassOneKeys() []string {
+func (ua *UserAgentsStatsProcessor) PassOneKeys() []string {
 	// this is used to fetch the dcs metadata, we send same get-config command to fetch the dc-names required in next steps
 	return nil
 }
 
-func (xw *UserAgentsStatsProcessor) PassTwoKeys(rawMetrics map[string]string) []string {
+func (ua *UserAgentsStatsProcessor) PassTwoKeys(rawMetrics map[string]string) []string {
 	log.Tracef("user-agent-passonekeys:%s", []string{USER_AGENTS})
-	return []string{KEY_XDR_METADATA}
+	return []string{USER_AGENTS}
 }
 
 // refresh prom metrics - parse the given rawMetrics (both config and stats ) and push to given channel
-func (xw *UserAgentsStatsProcessor) Refresh(infoKeys []string, rawMetrics map[string]string) ([]AerospikeStat, error) {
+func (ua *UserAgentsStatsProcessor) Refresh(infoKeys []string, rawMetrics map[string]string) ([]AerospikeStat, error) {
 
-	if xw.xdrMetrics == nil {
-		xw.xdrMetrics = make(map[string]AerospikeStat)
+	if ua.userAgentsMetrics == nil {
+		ua.userAgentsMetrics = make(map[string]AerospikeStat)
 	}
 
 	var allMetricsToSend = []AerospikeStat{}
 
 	for _, key := range infoKeys {
 
-		xdrRawMetrics := rawMetrics[key]
-		fmt.Println(xdrRawMetrics)
+		userAgentsMetrics := rawMetrics[key]
+		fmt.Println("key is ", key, " and userAgentsMetrics is ", userAgentsMetrics)
+		uaMetricsToSend, err := ua.handleRefresh(key, userAgentsMetrics)
+
+		if err != nil {
+			return nil, err
+		}
+
+		allMetricsToSend = append(allMetricsToSend, uaMetricsToSend...)
 	}
 
 	return allMetricsToSend, nil
+}
+
+func (ua *UserAgentsStatsProcessor) handleRefresh(infoKeyToProcess string, uaRawMetrics string) ([]AerospikeStat, error) {
+
+	stats := strings.Split(uaRawMetrics, ";")
+	fmt.Println("infoKeyToProcess is ", infoKeyToProcess, " and stats are ", stats)
+	var uaMetricsToSend = []AerospikeStat{}
+
+	for _, stat := range stats {
+		if len(stat) == 0 {
+			continue
+		}
+		userAgentVersion, clientLibraryVersion, appId, err := ua.getUserAgentInfo(stat)
+
+		if err != nil {
+			logrus.Error("Error decoding user agent client version: encoded value: ", stat, " error: ", err)
+			continue
+		}
+
+		// Count value
+		uaClientVersionCount := strings.ReplaceAll(strings.Split(stat, ":")[1], "count=", "")
+
+		pv, err := commons.TryConvert(uaClientVersionCount)
+		if err != nil {
+			logrus.Error("Error converting user agent client version count: ", uaClientVersionCount, " error: ", err)
+			continue
+		}
+
+		asMetric, exists := ua.userAgentsMetrics[stat]
+		dynamicStatname := "details"
+
+		if !exists {
+			allowed := isMetricAllowed(commons.CTX_USER_AGENTS, stat)
+			asMetric = NewAerospikeStat(commons.CTX_USER_AGENTS, dynamicStatname, allowed)
+			ua.userAgentsMetrics[stat] = asMetric
+		}
+
+		labels := []string{commons.METRIC_LABEL_CLUSTER_NAME, commons.METRIC_LABEL_SERVICE, commons.METRIC_LABEL_UA_VERSION, commons.METRIC_LABEL_UA_CLIENT_LIBRARY_VERSION, commons.METRIC_LABEL_UA_APP_ID}
+		labelValues := []string{ClusterName, Service, userAgentVersion, clientLibraryVersion, appId}
+
+		fmt.Println("userAgentVersion is ", userAgentVersion, " clientLibraryVersion is ", clientLibraryVersion, " appId is ", appId, " and uaClientVersionCount is ", pv)
+
+		asMetric.updateValues(pv, labels, labelValues)
+		uaMetricsToSend = append(uaMetricsToSend, asMetric)
+	}
+
+	return uaMetricsToSend, nil
+}
+
+func (ua *UserAgentsStatsProcessor) getUserAgentInfo(aaKeyWithAllInfo string) (string, string, string, error) {
+
+	//TODO: discuss on values to return
+	userAgentVersion, clientLibraryVersion, appId := "unknown", "unknown", "unknown"
+
+	uaKey := strings.ReplaceAll(strings.Split(aaKeyWithAllInfo, ":")[0], "user-agent=", "")
+	uaInfo, err := base64.StdEncoding.DecodeString(uaKey)
+
+	if err != nil {
+		logrus.Error("Error decoding user agent client version: encoded value: ", uaKey, " error: ", err)
+		return userAgentVersion, clientLibraryVersion, appId, err
+	}
+
+	uaInfoValues := strings.Split(string(uaInfo), ",")
+
+	// older clients, apps with no user-agent logic then we get "unknown" values
+	userAgentVersion = uaInfoValues[0]
+	if len(uaInfoValues) > 1 {
+		clientLibraryVersion = uaInfoValues[1]
+	}
+	if len(uaInfoValues) > 2 {
+		appId = uaInfoValues[2]
+	}
+
+	return userAgentVersion, clientLibraryVersion, appId, nil
 }
