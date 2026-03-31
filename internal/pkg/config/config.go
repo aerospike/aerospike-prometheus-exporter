39abbe89c34259a395f5d26e3444ef682e881c40
mphani <pmokrala@aerospike.com>
2026-03-31 13:43:25 +0530

    OM-268
    fixed bug, atleast prom or otel should be enabled.

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -206,6 +206,9 @@ func (c *Config) ValidateAndUpdate(md toml.MetaData) {
 	}
 
 	// If both Prom and Otel are not enabled, then error out
+	if !c.Agent.PrometheusEnabled && !c.Agent.OtelEnabled {
+		log.Fatalf("Atleast one of Prometheus and OpenTelemetry should be enabled")
+	}
 
 }
 
